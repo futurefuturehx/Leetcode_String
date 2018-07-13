f94b55a857980fc4b2fbefbb01d6e90366f0e38f
futurefuturehx <360574435@qq.com>
2018-07-13 11:03:20 +0800

    7/13

---
+++ 784Letter_Case_Permutation.cpp
@@ -0,0 +1,11 @@
+//
+// Created by 联想 on 2018/7/13.
+//
+//题意:输入一个字符串 把其中的字母转化成大小写的各种可能性
+//Take:小写a与大写A相差32 其实只要对于第五位进行移位操作就可以进行大小写转换了
+#include <iostream>
+using namespace std;
+
+int main(){
+
+}
